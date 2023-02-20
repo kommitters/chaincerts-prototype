d4bcc0564339f90128d0e6839e81c88f096c4e07
Mario Rodriguez <64106728+MarioRodriguezS@users.noreply.github.com>
2023-02-20 16:45:05 -0500

    remove tailwind and update the reamde with the comments

--- visualizer/src/App.tsx
+++ visualizer/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <div>
-        <h1 className="text-3xl font-bold underline text-center">Visualizer</h1>
+        <h1>Visualizer</h1>
       </div>
     </div>
   );
