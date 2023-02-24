672c1b2b828c556f04b4430160fbe6d632f991a9
Kelium Jojoa Urrutia <keliumju@gmail.com>
2023-02-24 15:52:40 -0500

    Update locales for EN

--- visualizer/src/App.tsx
+++ visualizer/src/App.tsx
@@ -1,18 +1,10 @@
-import { t } from 'i18next';
-import i18n from './i18n';
-import { I18nextProvider } from 'react-i18next';
-
 function App() {
   return (
-    <I18nextProvider i18n={i18n}>
-      <div className="App">
-        <div>
-          <h1>Visualizer</h1>
-          <p>{t('home_info.title')}</p>
-          <p>{t('home_info.information_paragraph')}</p>
-        </div>
+    <div className="App">
+      <div>
+        <h1>Visualizer</h1>
       </div>
-    </I18nextProvider>
+    </div>
   );
 }
 
