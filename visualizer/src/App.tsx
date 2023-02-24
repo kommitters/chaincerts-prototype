ac1f009f59b83a1fff4d1dc8a60b3a9ab68180fe
Kelium Jojoa Urrutia <keliumju@gmail.com>
2023-02-24 15:27:13 -0500

    Add setup for i18n and local in EN

--- visualizer/src/App.tsx
+++ visualizer/src/App.tsx
@@ -1,10 +1,18 @@
+import { t } from 'i18next';
+import i18n from './i18n';
+import { I18nextProvider } from 'react-i18next';
+
 function App() {
   return (
-    <div className="App">
-      <div>
-        <h1>Visualizer</h1>
+    <I18nextProvider i18n={i18n}>
+      <div className="App">
+        <div>
+          <h1>Visualizer</h1>
+          <p>{t('home_info.title')}</p>
+          <p>{t('home_info.information_paragraph')}</p>
+        </div>
       </div>
-    </div>
+    </I18nextProvider>
   );
 }
 
