1bbb72206b7c9bf289aeffdc39268dddaf3903a5
karolrojas07 <100703669+karolrojas07@users.noreply.github.com>
2023-02-28 15:05:41 -0500

    Create const file

---
+++ builder/src/configs/consts.ts
@@ -0,0 +1,6 @@
+export const NETWORK_PASSPHRASE = 'Test SDF Network ; September 2015';
+export const AMOUNT = '0.0000001';
+export const STARTING_BALANCE = '3';
+export const LOCK_ACCOUNT = 0;
+export const ISSUER_DATA_NAME = 'CID';
+export const HORIZON_TESTNET = 'https://horizon-testnet.stellar.org';
