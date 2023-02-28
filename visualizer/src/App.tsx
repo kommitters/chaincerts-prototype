004570302c267ac0d0d16c6ff3dda3e1861bd971
Mario Rodriguez <64106728+MarioRodriguezS@users.noreply.github.com>
2023-02-27 19:44:58 -0500

    add folder pages and set the routes

--- visualizer/src/App.tsx
+++ visualizer/src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Certificates from './pages/certificates';
+import Home from './pages/home';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route />
+          <Route path="/" element={<Home />} />
+          <Route path="/certificates/:stellar_key" element={<Certificates />} />
           <Route />
         </Routes>
       </Router>
