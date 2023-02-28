c9adc03bf2c2d48e4103d2c711d6bd0f8097bce6
Kelium Jojoa Urrutia <keliumju@gmail.com>
2023-02-27 19:04:39 -0500

    Refactor test and resolve comments

--- visualizer/src/pages/certificates/index.tsx
+++ visualizer/src/pages/certificates/index.tsx
@@ -1,5 +1,14 @@
+import Navbar from '../../components/navbar';
+import Profile from '../../components/profile';
+
 const Certificates = () => {
-  return <></>;
+  return (
+    <>
+      <h1>Certificates</h1>
+      <Navbar />
+      <Profile stellar_key="GDG5FFM2L534PEL4HQTTN2AR6P3E2TJYD5CTOD42DXKBIBBRAWSQAZLJV" />
+    </>
+  );
 };
 
 export default Certificates;
