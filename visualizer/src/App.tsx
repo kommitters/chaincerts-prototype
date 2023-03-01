3e97c5d9cdd5515fc50280d849b58a2121a589c8
Mario Rodriguez <64106728+MarioRodriguezS@users.noreply.github.com>
2023-03-01 10:28:18 -0500

    remove unnecesary code in app and pages

--- visualizer/src/App.tsx
+++ visualizer/src/App.tsx
@@ -1,23 +1,18 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Certificates from './pages/certificates';
 import Home from './pages/home';
-import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n';
-import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
-      <I18nextProvider i18n={i18n}>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/certificates/:stellar_key" element={<Certificates />} />
-            <Route path="/*" element={<Navigate replace to="/" />} />
-            <Route />
-          </Routes>
-        </Router>
-      </I18nextProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/certificates/:stellar_key" element={<Certificates />} />
+          <Route path="/*" element={<Navigate replace to="/" />} />
+          <Route />
+        </Routes>
+      </Router>
     </div>
   );
 }
