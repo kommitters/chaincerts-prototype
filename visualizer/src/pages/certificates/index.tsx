b97a0b656524e93d4e1d54bf95b8b5bc0d34c955
Mario Rodriguez <64106728+MarioRodriguezS@users.noreply.github.com>
2023-03-01 17:07:20 -0500

    remove no related code in certificates page and app.tsx

--- visualizer/src/pages/certificates/index.tsx
+++ visualizer/src/pages/certificates/index.tsx
@@ -1,14 +1,5 @@
-import Navbar from '../../components/navbar';
-import Profile from '../../components/profile';
-
 const Certificates = () => {
-  return (
-    <>
-      <h1>Certificates</h1>
-      <Navbar />
-      <Profile stellar_key="GDG5FFM2L534PEL4HQTTN2AR6P3E2TJYD5CTOD42DXKBIBBRAWSQAZLJV" />
-    </>
-  );
+  return <></>;
 };
 
 export default Certificates;
