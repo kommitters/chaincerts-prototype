d25ebeabc184a8f0ea9b1697a9a142d61a628dbc
Mario Rodriguez <64106728+MarioRodriguezS@users.noreply.github.com>
2023-03-01 10:33:10 -0500

    remove unnecesary file

--- visualizer/src/react-i18next.d.ts
+++
@@ -1,9 +0,0 @@
-import 'react-i18next';
-import en from './assets/locales/en/translation.json';
-
-declare module 'react-i18next' {
-  interface CustomTypeOptions {
-    defaultNS: 'common';
-    resources: typeof en;
-  }
-}
