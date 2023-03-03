90266f2fa6bffc2764cfde432dc35ac56777fc95
norn <48634254+keliumJU@users.noreply.github.com>
2023-03-03 16:17:12 -0500

    Build home page component (#48)
    * Add desing for home page
    
    * Add two public key to home desing
    
    * Updatae background image name
    
    * Refactor to single background image

--- visualizer/src/components/footer/Footer.tsx
+++ visualizer/src/components/footer/Footer.tsx
@@ -1,9 +1,10 @@
 import { t } from 'i18next';
 import kommitLogo from '/icons/kommitLogo.svg';
+import './styles.css';
 
 function Footer() {
   return (
-    <div className="footer">
+    <div className="footer-container">
       <img className="kommit-logo" src={kommitLogo} alt="kommit-logo" />
       <span className="footer-description">{t('footer.description')}</span>
     </div>
--- visualizer/src/pages/home/Home.tsx
+++ visualizer/src/pages/home/Home.tsx
@@ -1,5 +1,30 @@
+import { t } from 'i18next';
+
+import chaincertsLogo from '/static/icons/chaincertsLogo.svg';
+import MainInput from '../../components/mainInput';
+import Footer from '../../components/footer';
+import './styles.css';
+
 const Home = () => {
-  return <></>;
+  return (
+    <div className="home-container">
+      <div className="wrapper-home">
+        <img className="chaincerts-logo" src={chaincertsLogo} alt="chaincerts-logo" />
+        <div className="wrapper-main-home">
+          <span className="home-title">{t('home.title')}</span>
+          <p className="home-info">{t('home.info')}</p>
+          <span className="home-key">GB6KONY6F5U3HWSQSPCUX2HJPPENEBP5P77ALW3HPABUHP6YQNZUFZHW</span>
+          <span className="home-key">GCQXTWFTNV4ILHREYU7OBU3SYBGPF4EXFCI2GEB47CGYENN2AWC4OOBX</span>
+          <div className="home-input-container">
+            <MainInput />
+          </div>
+        </div>
+        <div className="footer-home">
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
