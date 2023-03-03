24090e3007e54cad70a1192137d4aefb50911a17
Kelium Jojoa Urrutia <keliumju@gmail.com>
2023-03-03 15:02:46 -0500

    Add two public key to home desing

--- visualizer/src/index.tsx
+++ visualizer/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './fonts.css';
 import '../buffer.config.js';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
--- visualizer/src/pages/home/Home.tsx
+++ visualizer/src/pages/home/Home.tsx
@@ -14,6 +14,7 @@ const Home = () => {
           <span className="home-title">{t('home.title')}</span>
           <p className="home-info">{t('home.info')}</p>
           <span className="home-key">GB6KONY6F5U3HWSQSPCUX2HJPPENEBP5P77ALW3HPABUHP6YQNZUFZHW</span>
+          <span className="home-key">GCQXTWFTNV4ILHREYU7OBU3SYBGPF4EXFCI2GEB47CGYENN2AWC4OOBX</span>
           <div className="home-input-container">
             <MainInput />
           </div>
