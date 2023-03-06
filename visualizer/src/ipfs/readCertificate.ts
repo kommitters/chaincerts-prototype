675f00e24996a838aa8f9e6210fbd3b7b3979897
Juan Manuel Giraldo <84339390+juanmagiraldor@users.noreply.github.com>
2023-03-06 14:48:14 -0500

    Fix certificates not loading (#62)

--- visualizer/src/ipfs/readCertificate.ts
+++ visualizer/src/ipfs/readCertificate.ts
@@ -4,5 +4,7 @@ export const readCertificate = (cid: string) => {
   return fetch(`${IPFS_BASE_URL}/${cid}`)
     .then((response) => response.json())
     .then((certificate) => certificate)
-    .catch((error) => console.log(error));
+    .catch((error) => {
+      throw new Error(error);
+    });
 };
