3ba87c001fe51cde4199ab7eda38377cbf546546
Kelium Jojoa Urrutia <keliumju@gmail.com>
2023-03-06 14:48:51 -0500

    Add desing for page certificates
    Co-authored-by: Juan Manuel Giraldo <juanmagiraldor@users.noreply.github.com>

--- visualizer/src/pages/certificates/Certificates.tsx
+++ visualizer/src/pages/certificates/Certificates.tsx
@@ -1,7 +1,11 @@
 import { useLocation } from 'react-router-dom';
+import { t } from 'i18next';
+
 import Carousel from '../../components/carousel/Carousel';
 import CertificateInformation from '../../components/certificateInformation/CertificateInformation';
 import { IAssetInformation } from '../../components/assetInformation/interfaces';
+import Profile from '../../components/profile';
+import Navbar from '../../components/navbar';
 
 const Certificates = () => {
   const { state: certificates } = useLocation();
@@ -10,7 +14,22 @@ const Certificates = () => {
     <CertificateInformation key={index} id={`certificate-${index}`} assetInformation={certificate} />
   ));
 
-  return <Carousel carouselData={carouselData}></Carousel>;
+  return (
+    <div className="certificates-wrapper">
+      <header>
+        <nav>
+          <Navbar />
+        </nav>
+        <div>
+          <Profile stellarKey="adslkj" />
+        </div>
+        <p>{t('certificates.description')}</p>
+      </header>
+      <main className="certificates-main">
+        <Carousel carouselData={carouselData}></Carousel>
+      </main>
+    </div>
+  );
 };
 
 export default Certificates;
