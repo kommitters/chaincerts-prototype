57485ecd9b841c160edcfec83c3203b0d7b87bc8
Kelium Jojoa Urrutia <keliumju@gmail.com>
2023-03-06 12:10:10 -0500

    Improve home page desing

--- visualizer/src/App.tsx
+++ visualizer/src/App.tsx
@@ -1,24 +1,43 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
+import { useState, useEffect } from 'react';
 
 import Certificates from './pages/certificates';
+import Footer from './components/footer';
 import Home from './pages/home';
 import i18n from './i18n';
 
-const App = () => {
+function App() {
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = 'static/images/backgroundImage.svg';
+    img.onload = () => {
+      setIsImageLoaded(true);
+    };
+  }, []);
+
   return (
-    <div className="app">
-      <I18nextProvider i18n={i18n}>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/certificates/:stellar_key" element={<Certificates />} />
-            <Route path="/*" element={<Navigate replace to="/" />} />
-          </Routes>
-        </Router>
-      </I18nextProvider>
-    </div>
+    <>
+      {isImageLoaded && (
+        <div className="app">
+          <I18nextProvider i18n={i18n}>
+            <Router>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/certificates/:stellar_key" element={<Certificates />} />
+                <Route path="/*" element={<Navigate replace to="/" />} />
+              </Routes>
+            </Router>
+          </I18nextProvider>
+          <div className="footer-page">
+            <Footer />
+          </div>
+        </div>
+      )}
+    </>
   );
-};
+}
 
 export default App;
--- visualizer/src/pages/home/Home.tsx
+++ visualizer/src/pages/home/Home.tsx
@@ -2,26 +2,18 @@ import { t } from 'i18next';
 
 import chaincertsLogo from '/static/icons/chaincertsLogo.svg';
 import MainInput from '../../components/mainInput';
-import Footer from '../../components/footer';
 import './styles.css';
 
 const Home = () => {
   return (
     <div className="home-container">
-      <div className="wrapper-home">
-        <img className="chaincerts-logo" src={chaincertsLogo} alt="chaincerts-logo" />
-        <div className="wrapper-main-home">
-          <span className="home-title">{t('home.title')}</span>
-          <p className="home-info">{t('home.info')}</p>
-          <span className="home-key">GAQFD264BREGU67VGWQ6JIBRQTGCZTGHEVL63ZV4IBWEMYHJQB7OOMID</span>
-          <span className="home-key">GBAAAUVZQQ6OAPU2IT5Z4WDQRLJ6AJFNPXL2SCZTA5TRB6FZBQMBJPV4</span>
-          <div className="home-input-container">
-            <MainInput />
-          </div>
-        </div>
-        <div className="footer-home">
-          <Footer />
-        </div>
+      <img className="chaincerts-logo" src={chaincertsLogo} alt="chaincerts-logo" />
+      <div className="wrapper-main-home">
+        <span className="home-title">{t('home.title')}</span>
+        <p className="home-info">{t('home.info')}</p>
+        <span className="home-key">GB6KONY6F5U3HWSQSPCUX2HJPPENEBP5P77ALW3HPABUHP6YQNZUFZHW&nbsp;&nbsp;</span>
+        <span className="home-key">GCQXTWFTNV4ILHREYU7OBU3SYBGPF4EXFCI2GEB47CGYENN2AWC4OOBX&nbsp;&nbsp;</span>
+        <MainInput />
       </div>
     </div>
   );
