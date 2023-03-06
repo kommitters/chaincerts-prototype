20886614f1261ee1ca79fd1b0a3386afd1558da8
Juan Manuel <juan.giraldo@kommit.co>
2023-03-06 10:36:32 -0500

    Implement certificate information component

--- visualizer/src/pages/certificates/Certificates.tsx
+++ visualizer/src/pages/certificates/Certificates.tsx
@@ -1,5 +1,14 @@
+import Carousel from '../../components/carousel/Carousel';
+import CertificateInformation from '../../components/certificateInformation/CertificateInformation';
+
+const data = [
+  <CertificateInformation key={1} id={'1'} />,
+  <CertificateInformation key={2} id={'2'} />,
+  <CertificateInformation key={5} id={'5'} />
+];
+
 const Certificates = () => {
-  return <></>;
+  return <Carousel carouselData={data}></Carousel>;
 };
 
 export default Certificates;
