8f5d89722cd0a39c6a57a548eba76cf8d1a43d9a
norn <48634254+keliumJU@users.noreply.github.com>
2023-03-07 08:31:47 -0500

    Taildwind and daisyui (#67)
    * Add setup for taildwind and daisyui
    
    * remove unnecessary dependencies

---
+++ visualizer/tailwind.config.cjs
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    'node_modules/daisyui/dist/**/*.js',
+    'node_modules/react-daisyui/dist/**/*.js'
+  ],
+
+  theme: {
+    extend: {}
+  },
+  plugins: [require('daisyui')]
+};
