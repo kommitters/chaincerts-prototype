72863278efa030aa732427a8a871d63554794048
einerzg <einerzg@gmail.com>
2023-03-08 10:35:07 -0500

    Improve background image loading

--- visualizer/src/App.tsx
+++ visualizer/src/App.tsx
@@ -1,46 +1,33 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
-import { useState, useEffect } from 'react';
 
 import Certificates from './pages/certificates';
 import Footer from './components/footer';
 import Home from './pages/home';
 import i18n from './i18n';
 
-const URL_BACKGROUND_IMAGE = '/images/backgroundImage.svg';
+const URL_BACKGROUND_IMAGE = '/images/backgroundImage.jpg';
 
 function App() {
-  const [isImageLoaded, setIsImageLoaded] = useState(false);
-
-  useEffect(() => {
-    const img = new Image();
-    img.src = URL_BACKGROUND_IMAGE;
-    img.onload = () => {
-      setIsImageLoaded(true);
-    };
-  }, []);
-
   return (
     <>
-      {isImageLoaded && (
-        <div
-          className="min-h-screen bg-cover bg-center text-white"
-          style={{ backgroundImage: `url(${URL_BACKGROUND_IMAGE})` }}
-        >
-          <div className="container mx-auto px-4">
-            <I18nextProvider i18n={i18n}>
-              <Router>
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/certificates/:stellar_key" element={<Certificates />} />
-                  <Route path="/*" element={<Navigate replace to="/" />} />
-                </Routes>
-              </Router>
-            </I18nextProvider>
-            <Footer />
-          </div>
+      <div
+        className="min-h-screen bg-cover bg-center text-white"
+        style={{ backgroundImage: `url(${URL_BACKGROUND_IMAGE})` }}
+      >
+        <div className="container mx-auto px-4">
+          <I18nextProvider i18n={i18n}>
+            <Router>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/certificates/:stellar_key" element={<Certificates />} />
+                <Route path="/*" element={<Navigate replace to="/" />} />
+              </Routes>
+            </Router>
+          </I18nextProvider>
+          <Footer />
         </div>
-      )}
+      </div>
     </>
   );
 }
